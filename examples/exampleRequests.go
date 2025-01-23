6e08c73cc62165a7e6f3f9643fcb5a478df18c80
yan00s <malayaaa3@gmail.com>
2025-01-23 16:14:38 +0200

    Refactored examples

---
+++ examples/exampleRequests.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	session "github.com/yan00s/go-session-client"
+)
+
+func main() {
+	ses := session.CreateSession(true)
+
+	/// Trying to set Proxy
+	proxyStr := "http://username:passw@100.100.100.100:2000"
+	timeout := 3 // seconds
+	if err := ses.SetProxy(proxyStr, timeout); err != nil {
+		fmt.Println(fmt.Errorf("Error in setting up proxy: %w", err))
+	}
+	///
+
+	fmt.Println()
+
+	/// Trying to make a get request
+	fmt.Println("Trying to make request with 1 try on icanhazip.com with a 10 second timeout per request")
+	resp := ses.SendReq("https://icanhazip.com", "GET", 1, 10*time.Second, 0)
+
+	if resp.Err != nil {
+		fmt.Println("Error in making request:", resp.Err.Error())
+		return
+	}
+
+	fmt.Println("Response:", resp.String())
+	fmt.Println("Status:", resp.Status)
+	///
+
+}
